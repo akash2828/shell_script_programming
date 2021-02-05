f763728748d50b5726ff7205085e7ccf900d7ce8
akash2828 <cu.16bcs2828@gmail.com>
2021-02-05 11:25:10 +0530

    command

---
+++ command.sh
@@ -0,0 +1,16 @@
+cd-change directory
+pwd-show directory
+ls-list
+cp-copy
+rm-remove
+mv-move
+echo-print
+cat-concatination
+mkdir-create directory
+less
+grep
+touch- to make files very quickly
+chmod-change mode
+
+
+
